fdeebdbfb488762b07fc5437a3fe0ab223bd83ca
VLN37 <jfpr1237@gmail.com>
2023-10-11 23:51:31 -0300

    feat: strlcpy optimizations

--- src/ft_strlcpy.c
+++ src/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: jofelipe <jofelipe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/09 11:26:42 by jofelipe          #+#    #+#             */
-/*   Updated: 2021/08/02 13:13:49 by jofelipe         ###   ########.fr       */
+/*   Updated: 2023/10/12 00:46:19 by jofelipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,29 @@
  * @param size the maximum amount of bytes that should be copied
  * @return size_t - the length of SRC
  */
+
 size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
-	size_t	i;
+	size_t	srclen;
 
-	if (!dest || !src)
-		return (0);
-	if (size < 1)
-		return (ft_strlen(src));
-	i = 0;
-	while (src[i] && i < size - 1)
+	srclen = ft_strlen(src);
+	if (!size)
+		return (srclen);
+	if (srclen < size - 1)
+		size = srclen + 1;
+	while (size > 8)
 	{
-		dest[i] = src[i];
-		i++;
+		*(uint64_t *)dest = *(uint64_t const *)src;
+		dest += 8;
+		src += 8;
+		size -= 8;
 	}
-	if (size > 0)
-		dest[i] = '\0';
-	return (ft_strlen(src));
+	while (size > 1)
+	{
+		*dest++ = *src++;
+		size--;
+	}
+	if (size)
+		*dest = '\0';
+	return (srclen);
 }
