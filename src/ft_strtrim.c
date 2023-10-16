7129af67b5c9c09d9bc3d2535d69b9a1c60bbffc
VLN37 <jfpr1237@gmail.com>
2023-10-16 20:08:48 -0300

    refactor: simplify ft_strtrim

--- src/ft_strtrim.c
+++ src/ft_strtrim.c
@@ -6,54 +6,11 @@
 /*   By: jofelipe <jofelipe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 10:21:45 by jofelipe          #+#    #+#             */
-/*   Updated: 2021/09/21 11:53:50 by jofelipe         ###   ########.fr       */
+/*   Updated: 2023/10/16 20:08:25 by jofelipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static int	istrimmed(char c, char const *set)
-{
-	while (*set)
-	{
-		if (c == *set)
-			return (1);
-		set++;
-	}
-	return (0);
-}
-
-/**
- * @brief determines the starting parameters for trim to work
- *
- * @param size dereferences the amount of bytes that will be written
- * @param start dereferences the starting index to work with
- */
-static void	setvariables(size_t *size, size_t *start,
-char const *s1, char const *set)
-{
-	int	i;
-
-	i = 0;
-	if (!s1)
-		return ;
-	*size = ft_strlen((char *)s1);
-	while (istrimmed(s1[i], set))
-	{
-		*size -= 1;
-		i++;
-	}
-	*start = i;
-	i = ft_strlen((char *)s1);
-	if (*size > 1)
-	{
-		while (istrimmed(s1[i - 1], set))
-		{
-			*size -= 1;
-			i--;
-		}
-	}
-}
 
 /**
  * @brief trims SET from the start and end of the string s1, returns a freshly
@@ -66,15 +23,20 @@ allocated string with the remaining characters
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t		size;
-	size_t		start;
 	char		*res;
+	char		*end;
 
 	if (!s1)
 		return (NULL);
-	setvariables(&size, &start, s1, set);
-	res = (char *)ft_calloc(size + 1, sizeof(char));
+	end = (char *)s1 + ft_strlen(s1) - 1;
+	while (ft_strchr(set, *s1) && *s1)
+		s1++;
+	while (ft_strchr(set, *end) && end > s1)
+		end--;
+	size = end - s1 + 2;
+	res = malloc(size * sizeof(char));
 	if (!res)
 		return (NULL);
-	ft_strlcpy(res, &s1[start], size + 1);
+	ft_strlcpy(res, s1, size);
 	return (res);
 }
