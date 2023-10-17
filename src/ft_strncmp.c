8839bcaaa2bb5713d9483186816894050a3ac5f4
VLN37 <jfpr1237@gmail.com>
2023-10-17 12:38:37 -0300

    refactor: simpliy ft_strncmp

--- src/ft_strncmp.c
+++ src/ft_strncmp.c
@@ -6,33 +6,28 @@
 /*   By: jofelipe <jofelipe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/13 04:45:18 by jofelipe          #+#    #+#             */
-/*   Updated: 2021/08/02 13:17:51 by jofelipe         ###   ########.fr       */
+/*   Updated: 2023/10/17 12:54:31 by jofelipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	char_compare(unsigned char c1, unsigned char c2)
+{
+	return (c1 - c2);
+}
+
 //returns the difference of the first character that differ between
 //s1 and s2, interpreted as unsigned char
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned int	i;
+	size_t	i;
 
-	i = 0;
-	while ((s1[i] != '\0' && s2[i] != '\0') && i < n)
-	{
+	i = -1;
+	while (s1[++i] && i < n)
 		if (s1[i] != s2[i])
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-		i++;
-	}
-	while (i < n)
-	{
-		if (s1[i] == '\0' && s2[i] != '\0')
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-		else if (s1[i] != '\0' && s2[i] == '\0')
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-		else
-			return (0);
-	}
+			return (char_compare(s1[i], s2[i]));
+	if (i < n && s1[i] != s2[i])
+		return (char_compare(s1[i], s2[i]));
 	return (0);
 }
